2059ba4102df0a7645277a88794aa8531bd5f974
xnl219 <xnl219@lehigh.edu>
2015-08-30 20:36:37 +0000

    undated WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,21 @@
+//Xavier Lema
+//hw01
+//WelcomeClass
+
+//Class
+public class WelcomeClass{
+    
+    //Method
+    public static void  main(String[] args) {
+        
+// Printed Staments
+    System.out.println("     -----------    ");
+    System.out.println("     | WELCOME |    ");
+    System.out.println("  ^  ^  ^  ^  ^  ^  ");
+    System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\ ");
+    System.out.println("<-X--N--L--2--1--9->");
+    System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ / ");
+    System.out.println("  v  v  v  v  v  v  ");
+    System.out.println("This summer my friend Tevon and I went on a month long trip to Europe.");
+    }
+}
