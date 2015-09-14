2c01d46509f225edb6392d678b5b74d06c97b80f
xnl219 <xnl219@lehigh.edu>
2015-09-14 19:04:08 +0000

    updated hw03

--- hw03/Block.java
+++ hw03/Block.java
@@ -3,29 +3,29 @@
 //user inputs lenght, height, and width
 //program calculates and prints the volume and surface area
 
-
+//scanner
 import java.util.Scanner;
-
+//class
 public class Block {
     
     public static void main(String[] args)  {
-        
+        //create scanner
         Scanner blockScanner = new Scanner( System.in );
-        
+        //input and call length
         System.out.print("Enter the length of the block: ");
         double length = blockScanner.nextDouble ();
-        
+        //input and call width
         System.out.print("Enter the width of the block: ");
         double width = blockScanner.nextDouble ();
-        
+        //input and call height
         System.out.print("Enter the height of the block: ");
         double height = blockScanner.nextDouble ();
-        
+        //volume and surface area variables
         double volume, surfaceArea;
-        
+        //calculations
         surfaceArea = (length*height*2)+(length*width*2)+(height*width*2);
         volume = length*height*width;
-        
+        //print results
         System.out.println("The volume of the block of dimensions "+length+" X "+width+" X "+height+" is "+volume);
         System.out.println("The surface areas of the block is "+surfaceArea);
     }
