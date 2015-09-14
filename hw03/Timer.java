bbfd509c4c9c6af49c1dbd69a4f4a5a587b4f12a
xnl219 <xnl219@lehigh.edu>
2015-09-14 18:54:45 +0000

    updated hw03

--- hw03/Timer.java
+++ hw03/Timer.java
@@ -8,21 +8,55 @@ public class Timer {
         //Scanner setup
         Scanner timerScanner = new Scanner( System.in );
         //promp user for current time
-        System.out.println("press return after every digit");
-        System.out.println("Enter the current time in the form (HH:MM) : ");
-        
+        System.out.print("Enter the current time (HHMM) : ");
         //input current time
-        double currentTime = timerScanner.nextDouble ();
+        int currentTime = timerScanner.nextInt ();
         //promp user for dinner time
         System.out.print("Enter desired dinner time (HHMM) : ");
         //input dinner time
-        double dinnerTime = timerScanner.nextDouble ();
+        int dinnerTime = timerScanner.nextInt ();
         
-        //Time until dinner variable
-        double timeLeft;
-        int hours, minutes;
+        //minute and hour variables and calculations for current time
+        int currentTimeHour = (currentTime/100);
+        int currentTimeMinute = (currentTime-(currentTimeHour*100));
+        //minute and hour variable and calculations for dinner time
+        int dinnerTimeHour = (dinnerTime/100);
+        int dinnerTimeMinute = (dinnerTime-(dinnerTimeHour*100));
+        //final hours and minutes left variables
+        int hoursLeft, minutesLeft;
         
-         
+        //solves next day possability issue
+        if (dinnerTimeHour <= currentTimeHour) {
+            dinnerTimeHour = dinnerTimeHour + 24;
+        }
+        
+        //final calculations
+        hoursLeft = dinnerTimeHour-currentTimeHour;
+        minutesLeft = dinnerTimeMinute-currentTimeMinute;
+        
+       //resolves negative results 
+        if (minutesLeft < 0) {
+            hoursLeft = hoursLeft-1;
+            minutesLeft = (Math.abs(minutesLeft));
+        }
+        
+        //string variables hours and minunutes
+        String hours = " hours ";
+        String minutes = " minutes ";
+        
+        //resolves issue of plural vs singular grammar
+        if (hoursLeft == 1) {
+            hours = " hour ";
+        }
+        
+        if (minutesLeft == 1) {
+            minutes = " minute ";
+        }
+        
+        
+        //printing final out come
+        System.out.print("You have "+hoursLeft+hours+"and "+minutesLeft+minutes+"until dinner.");
+        
     }
     
 }
